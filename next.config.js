07777351c11b7f7c0e84088f24eb9f11cdc2eea8
Nitin <nitin@Nitins-MacBook-Air.local>
2023-10-06 22:56:55 +0530

    fix error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+    // Other Next.js configurations...
+  
+    // Enable static HTML export
+    output: 'export',
+  };
+
+  
